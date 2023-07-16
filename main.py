681ee9902a3049f0aaf9b823da966e2f4d438ac5
Aryann15 <rajlysm15@gmail.com>
2023-07-17 03:55:58 +0530

    sudoku grid detection

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def get_contours(img,original_img):
     contours,hierarchy = cv.findContours(img,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv.contourArea(cnt)
-        cv.drawContours(original_img , cnt , -1 . (0,255,0),3)
-
+        if area > 50000:
+            cv.drawContours(original_img ,cnt,-1,(0,255,0),2)
 
 
 while True:
@@ -21,8 +21,9 @@ while True:
     canny = cv.Canny(blur,50,50)
     copy = img.copy()
 
+    get_contours(canny,copy)
 
-    cv.imshow('webcam',img)
+    cv.imshow('webcam',copy)
     if cv.waitKey(1) & 0xff == ord('q'):
         break
 
