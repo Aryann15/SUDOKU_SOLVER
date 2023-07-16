256403fec77ccfdd9ba4c68aec77cf88a682b937
Aryann15 <rajlysm15@gmail.com>
2023-07-17 03:48:02 +0530

    function to get the contours

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ frame = cv.VideoCapture(0)
 frame.set(3,640)
 frame.set(4,480)
 
+def get_contours(img,original_img):
+    contours,hierarchy = cv.findContours(img,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv.contourArea(cnt)
+        cv.drawContours(original_img , cnt , -1 . (0,255,0),3)
+
+
+
 while True:
     success , img = frame.read()
     gray = cv.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray,(5,5),3)
     canny = cv.Canny(blur,50,50)
     copy = img.copy()
+
+
     cv.imshow('webcam',img)
     if cv.waitKey(1) & 0xff == ord('q'):
         break
