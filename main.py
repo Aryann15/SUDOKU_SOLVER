7d475082d3386f73f86b60bfe582ec63a372d903
Aryann15 <rajlysm15@gmail.com>
2023-07-17 03:42:23 +0530

    gray and gausssian blur

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2
 import cv2 as cv
 
 frame = cv.VideoCapture(0)
@@ -7,6 +8,10 @@ frame.set(4,480)
 
 while True:
     success , img = frame.read()
+    imgGray = cv.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv.GaussianBlur(imgGray,(5,5),3)
     cv.imshow('webcam',img)
     if cv.waitKey(1) & 0xff == ord('q'):
         break
+
+
