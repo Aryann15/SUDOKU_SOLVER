3c3bbd9123f211e046ffea325245222b41c4a670
Aryann15 <rajlysm15@gmail.com>
2023-07-17 03:34:33 +0530

    webcam settings

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ frame = cv.VideoCapture(0)
 frame.set(3,640)
 frame.set(4,480)
 
+while True:
+    success , img = frame.read()
+    cv.imshow('webcam',img)
+    if cv.waitKey(1) & 0xff == ord('q'):
+        break
