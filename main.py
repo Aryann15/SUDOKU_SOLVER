1c5a31a471d9508c70af5668292f6002bccd8707
Aryann15 <rajlysm15@gmail.com>
2023-07-17 12:56:15 +0530

    defining the 8 points

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import cv2 as cv
+import numpy as np
 
 frame = cv.VideoCapture(0)
 
@@ -12,6 +13,21 @@ def get_contours(img,original_img):
         area = cv.contourArea(cnt)
         if area > 30000:
             cv.drawContours(original_img ,cnt,-1,(0,255,0),2)
+            peri = cv.arcLength(cnt,True)
+            approx = cv.approxPolyDP(cnt,0.02*peri,True)
+            ax = approx.item(0)
+            ay = approx.item(1)
+            bx = approx.item(2)
+            by = approx.item(3)
+            cx = approx.item(4)
+            cy = approx.item(5)
+            dx = approx.item(6)
+            dy = approx.item(7)
+
+            width,height = 900,900
+
+            pts1 = np.float32([[bx, by], [ax, ay], [cx, cy], [dx, dy]])
+            pts2 = np.float32([[0,0], [width,0], [0,height], [width,height]])
 
 
 while True:
