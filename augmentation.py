9ed4e85722db5cacda9b8b93b3e099ca6c28971e
Aryann15 <rajlysm15@gmail.com>
2023-07-23 18:14:20 +0530

    cropping image

--- augmentation.py
+++ augmentation.py
@@ -14,10 +14,21 @@ def data_augmentation():
         for img_filename in (os.listdir(path)):
             try:
                 img_array = cv.imread(os.path.join(path,img_filename))
-                # plt.imshow(img_array,cmap= 'gray')
-                # plt.show()
                 canny = cv.Canny(img_array,50,50)
                 contours, hierarchy = cv.findContours(canny,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+
+                for cnt in contours:
+                    area = cv.contourArea(cnt)
+                    if area > 5:
+                        peri = cv.arcLength(cnt,True)
+                        approx= cv. approxPolyDP(cnt,0.02*peri,True)
+                        x,y,w,h = cv.boundingRect(approx)
+                        img_rect = img_array[y:y+h,x:x+w]
+                        img_rect = cv.resize(img_rect,(100,100))
+                        # plt.imshow(img_array,cmap= 'gray')
+                        # plt.show()
+
+
 
 
 
