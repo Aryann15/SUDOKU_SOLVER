1ec6acb97bc41d8bbae9e6d5771cd85f621140fa
Aryann15 <rajlysm15@gmail.com>
2023-07-23 20:54:04 +0530

    training model

--- augmentation.py
+++ augmentation.py
@@ -3,6 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import random
+import tensorflow as tf
+
+from statistics import *
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.preprocessing.image import img_to_array
+
 
 myData = 'C://Users//rajly//sudokuSOLVER//data'
 categories = ['0','1','2','3','4','5','6','7','8','9']
@@ -80,3 +92,17 @@ X = X.astype('float32')
 X = X /255.0
 
 Y = np.array(Y)
+
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X,Y,epochs=5)
+model.save('sudoku_model')
