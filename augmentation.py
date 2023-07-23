2e77e76df62e5e120821459990b712ad4fc0ad57
Aryann15 <rajlysm15@gmail.com>
2023-07-23 17:56:08 +0530

    location of the data

--- augmentation.py
+++ augmentation.py
@@ -3,7 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-myData = 'data'
+myData = 'C://Users//rajly//sudokuSOLVER//data'
 categories = ['0','1','2','3','4','5','6','7','8','9']
 
 
+def data_augmentation():
+    for category in categories:
+        path = os.path.join(myData,category)
+        class_num = categories.index(category)
+
+        for img_filename in (os.listdir(path)):
+            try:
+                img_array = cv.imread(os.path.join(path,img_filename))
+                plt.imshow(img_array,cmap= 'gray')
+                plt.show()
+
+            except Exception as e:
+                raise(e)
+
+data_augmentation()
